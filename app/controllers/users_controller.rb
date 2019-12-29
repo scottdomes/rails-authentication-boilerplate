1ebd0aab4e749c798dabac720339989308fa77b3
Scott Domes <sdomes@indeed.com>
2019-12-28 17:54:59 -0800

    Send auth token on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,11 @@ class UsersController < ApplicationController
   def login
     user = User.find_by(email: user_params[:email].to_s.downcase)
 
-    if user && user.authenticate(user_params[:password])
-        auth_token = JsonWebToken.encode({user_id: user.id})
-        render json: {auth_token: auth_token}, status: :ok
+    if user&.authenticate(user_params[:password])
+      auth_token = JsonWebToken.encode(user_id: user.id)
+      render json: { auth_token: auth_token }, status: :ok
     else
-      render json: {error: 'Invalid username / password'}, status: :unauthorized
+      render json: { error: 'Invalid username/password' }, status: :unauthorized
     end
   end
 
@@ -29,8 +29,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if @user.save && @user.authenticate(user_params[:password])
+      auth_token = JsonWebToken.encode(user_id: @user.id)
+      render json: { auth_token: auth_token }, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -51,13 +52,14 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
